df97ccbb5cfb6960fa58251388425e9df7eb97a5
te-arai <Stew_gibbs@hotmail.com>
2025-12-01 16:18:29 +1300

    Add files via upload

---
+++ run_app.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "C:\Users\stew_\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.13_qbz5n2kfra8p0\LocalCache\local-packages\Python313\site-packages"
+python -m streamlit run app.py
